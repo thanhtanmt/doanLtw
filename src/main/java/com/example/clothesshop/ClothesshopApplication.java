c70677e8274b0e525a5826e3cabedbc82179b9a7
TanLe <tanle20022005@gmail.com>
2025-10-13 17:58:49 +0700

    Initial commit

---
+++ src/main/java/com/example/clothesshop/ClothesshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.clothesshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothesshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothesshopApplication.class, args);
+	}
+
+}
